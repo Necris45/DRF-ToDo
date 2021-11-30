edecfc4bd3a77f981ea441322035cfa24f616826
Necris45 <necris01@mail.ru>
2021-11-30 15:18:38 +0500

    Homework №4 done

--- ToDo/settings.py
+++ ToDo/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'users',
     'corsheaders',
     'projects',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -138,12 +139,13 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
         'djangorestframework_camel_case.render.CamelCaseJSONRenderer',
         'djangorestframework_camel_case.render.CamelCaseBrowsableAPIRenderer',
-        # 'rest_framework.renderers.JSONRenderer',
-        # 'rest_framework.renderers.BrowsableAPIRenderer',
     ],
     'DEFAULT_PARSER_CLASSES': [
         'djangorestframework_camel_case.parser.CamelCaseFormParser',
         'djangorestframework_camel_case.parser.CamelCaseMultiPartParser',
         'djangorestframework_camel_case.parser.CamelCaseJSONParser',
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100,
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
--- projects/views.py
+++ projects/views.py
@@ -1,15 +1,40 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
-from projects.models import Project, Todo
-from projects.serialiazers import ProjectModelSerializer, TodoModelSerializer
+from .models import Project, Todo
+from .serialiazers import ProjectModelSerializer, TodoModelSerializer
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.renderers import BrowsableAPIRenderer, JSONRenderer
+from .filters import ProjectsFilter, TaskFilter
 # Create your views here.
+
+
+class ProjectsLimitPagination(LimitOffsetPagination):
+    default_limit = 10
+
+
+class TaskLimitPagination(LimitOffsetPagination):
+    default_limit = 20
 
 
 class ProjectModelViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectModelSerializer
+    renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
+    pagination_class = ProjectsLimitPagination
+    filterset_class = ProjectsFilter
 
 
 class TodoModelViewSet(ModelViewSet):
     queryset = Todo.objects.all()
     serializer_class = TodoModelSerializer
+    renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
+    pagination_class = TaskLimitPagination
+    filterset_class = TaskFilter
+
+    def delete(self, request, *args, ** kwargs):
+        self.object = self.get_object()
+        if self.object.is_active:
+            self.object.is_active = False
+        else:
+            self.object.is_active = True
+        self.object.save()
