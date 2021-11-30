9522844402b2712cb52ac96ddafa57d181bf6333
Necris45 <necris01@mail.ru>
2021-11-30 16:07:48 +0500

    fix filtering ToDo by date

--- projects/filters.py
+++ projects/filters.py
@@ -12,13 +12,14 @@ class ProjectsFilter(FilterSet):
 
 class TaskFilter(FilterSet):
     project = CharFilter()
-    created_at = NumberFilter(field_name='created_at', lookup_expr='day')
+    created_at_year = NumberFilter(field_name='created_at', lookup_expr='year')
+    created_at_month = NumberFilter(field_name='created_at', lookup_expr='month')
     # gte и lte использую, чтобы можно было выбрать конкретную дату в одном из двух полей и не потерять в поисковой
     # выдаче данные за указанный день, очень понравилась подсказка, которую рисует Django. Также в планах реализовать
     # поиск по пользователю, но позднее.
-    created_at__gt = NumberFilter(field_name='created_at', lookup_expr='day__gte')
-    created_at__lt = NumberFilter(field_name='created_at', lookup_expr='day__lte')
+    created_at_day__gt = NumberFilter(field_name='created_at', lookup_expr='day__gte')
+    created_at_day__lt = NumberFilter(field_name='created_at', lookup_expr='day__lte')
 
     class Meta:
         model = Todo
-        fields = ['project', 'created_at']
+        fields = ['project']
