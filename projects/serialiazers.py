542dfec637d74173db1e6a3c59ae8e42c45fccfd
Necris45 <necris01@mail.ru>
2021-12-30 13:26:50 +0500

    Try complete work with Docker

---
+++ projects/serialiazers.py
@@ -0,0 +1,14 @@
+from rest_framework.serializers import ModelSerializer
+from projects.models import Project, Todo
+
+
+class ProjectModelSerializer(ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ('id', 'name', 'repo', 'users',)
+
+
+class TodoModelSerializer(ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = ('id', 'project', 'task_text', 'created_at', 'updated_at', 'executive_user', 'is_active',)
