8dfa886adec8185acb754085daac69084e17feb3
Necris45 <necris01@mail.ru>
2021-12-30 21:51:00 +0500

    all dockers in docker-compose.yml

--- ToDo/users/management/commands/create_data.py
+++ ToDo/users/management/commands/create_data.py
@@ -11,9 +11,12 @@ class Command(BaseCommand):
         User.objects.create_user(username='Test_developer2', email='test_developer2@mail.ru', password='Qwerty1')
         User.objects.create_user(username='Test_manager2', email='test_manager2@mail.ru', password='Qwerty1')
 
-        Project.objects.create(name='Первый проект', repo="", users = [1,2,4])
-        Project.objects.create(name='Второй проект', repo="", users = [1,3,4])
-        Project.objects.create(name='Третий проект', repo="", users = [2,3,4])
+        project_1 = Project.objects.create(name='Первый проект', repo="")
+        project_1.users.set([1,2,3])
+        project_2 = Project.objects.create(name='Второй проект', repo="", users = [1,3,4])
+        project_2.users.set([1,3,4])
+        project_3 = Project.objects.create(name='Третий проект', repo="", users = [2,3,4])
+        project_3.users.set([2,3,4])
 
         Todo.objects.create(project=1, executive_user=2,task_text='первая задача')
         Todo.objects.create(project=2, executive_user=3, task_text='Вторая задача')
