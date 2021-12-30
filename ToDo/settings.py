50b5e51cf74ba7274d8c25b83ccfc5bf00716263
Necris45 <necris01@mail.ru>
2021-12-30 22:32:56 +0500

    all dockers in docker-compose.yml

--- ToDo/settings.py
+++ ToDo/settings.py
@@ -160,7 +160,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         # 'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAuthenticatedOrAnonReadOnly',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
