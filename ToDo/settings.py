24180e5ec179f607f00ff08866f1d6f022dd0926
Necris45 <necris01@mail.ru>
2021-12-30 23:17:32 +0500

    done

--- ToDo/settings.py
+++ ToDo/settings.py
@@ -160,7 +160,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         # 'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
-        'rest_framework.permissions.IsAuthenticatedOrAnonReadOnly',
+        'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
